922d325894bcee6142665b7e40577989d5a4d1c1
schumannlee <verti.schumann@gmail.com>
2026-03-17 14:30:08 -0300

    SCRUD-7: Create Models and Migrations for Plans, Contracts and Invoices

---
+++ database/migrations/2026_03_17_172435_create_plans_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Nome do plano (Ex: Prata, Ouro)
+            $table->text('description')->nullable(); // Serviços inclusos
+            $table->decimal('price', 10, 2); // Preço do plano
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plans');
+    }
+};
---
+++ database/migrations/2026_03_17_172502_create_contracts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contracts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Dono do contrato
+            $table->foreignId('plan_id')->constrained(); // Plano escolhido
+            $table->date('start_date');
+            $table->enum('status', ['active', 'inactive', 'suspended'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contracts');
+    }
+};
