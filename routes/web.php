364904c2f1e364c07e132d404f5015fc1b9cd590
schumannlee <verti.schumann@gmail.com>
2026-03-17 15:32:21 -0300

    SCRUD-8: Fix UserController import in web.php and clear route cache

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlanController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
